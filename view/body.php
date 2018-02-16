0fbed7942cfd7496c58945c3501514c2a3212108
m4rkos <marcos.j.eduardo@gmail.com>
2018-02-16 20:20:01 -0200

    Commit Atualização de layout | 16/02/2018 - 20:19h

--- view/body.php
+++ view/body.php
@@ -1,6 +1,6 @@
 <div>
     <div class="container">
-        <h3>"BODY"</h3>
+        <h3><?php echo date('Y');?></h3>
         <br>
         <a href="./old/" class="btn btn-primary">VERSÃO ANTIGA</a>
     </div>
